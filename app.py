da9618ec20856ddcdbd11c80de78906560702509
minhtr0921 <minhtri21092k1@gmail.com>
2025-01-10 10:39:49 +0700

    Fix for Render deployment

--- app.py
+++ app.py
@@ -70,5 +70,6 @@ async def Chat(item: Item):
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run("app:app")
+    import os
+    port = int(os.environ.get("PORT", 8000))  
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
